24a10724acf404b03c960c1578d64665db6bbc74
MiracleWong <cfwr1991@126.com>
2018-11-08 15:22:58 +0800

    使用Bootstrap-Flask简化页面编写

--- SayHello/__init__.py
+++ SayHello/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
-from flask_sqlalchemy import  SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask('SayHello')
 app.config.from_pyfile('settings.py')
@@ -8,5 +9,6 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 from SayHello import commands, errors, views
 
