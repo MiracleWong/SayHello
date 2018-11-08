345526c978f96b06f6dd68194f0727f04b50bdc5
MiracleWong <cfwr1991@126.com>
2018-11-08 11:42:26 +0800

    创建表单类和编写视图函数

---
+++ SayHello/forms.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+class HelloForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(1, 20)])
+    body = TextAreaField('Message', validators=[DataRequired(), Length(1, 200)])
+    submit = SubmitField()
--- SayHello/views.py
+++ SayHello/views.py
@@ -1,1 +1,22 @@
 # -*- coding:utf-8 -*-
+
+from flask import flash, redirect, url_for, render_template
+
+from SayHello import db, app
+from SayHello.models import Message
+from SayHello.forms import HelloForm
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    messages = Message.query.order_by(Message.timestamp.desc()).all()
+    form = HelloForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        body = form.body.data
+        message = Message(name=name, body=body) # 实例化模型
+        db.session.add(message)
+        db.session.commit()
+        flash('Your message have sent to the world!')
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, messages=messages)
