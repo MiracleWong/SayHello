e9f6700d91406c85ae25e7e9033fda24ec641f9f
MiracleWong <cfwr1991@126.com>
2018-11-08 16:04:07 +0800

    使用Flask-Moment本地化时间和日期

--- SayHello/__init__.py
+++ SayHello/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask('SayHello')
 app.config.from_pyfile('settings.py')
@@ -10,5 +11,7 @@ app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 from SayHello import commands, errors, views
 
