aff0cf8ae45ec6346ae901376bb4935d398514be
MiracleWong <cfwr1991@126.com>
2018-11-08 16:31:15 +0800

    使用Flask-DebugToolBar调试程序

--- SayHello/__init__.py
+++ SayHello/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask('SayHello')
 app.config.from_pyfile('settings.py')
@@ -12,6 +13,7 @@ app.jinja_env.lstrip_blocks = True
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+toolbar = DebugToolbarExtension(app)
 
 from SayHello import commands, errors, views
 
